946ebd6851bab1edd6542aef957af0a1fb20fe39
ivanb <bileichuk.ivan@gmail.com>
2024-08-28 17:30:27 +0300

    Aded temperature & city apps routers to main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
+from city_app.router import router as city_router
+from temperature_app.router import router as temperature_router
+
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(city_router)
+app.include_router(temperature_router)
