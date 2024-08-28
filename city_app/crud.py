ab63504369d445838187d59b133e3fb710b672c5
ivanb <bileichuk.ivan@gmail.com>
2024-08-28 15:21:50 +0300

    Aded crud for city_app

---
+++ city_app/crud.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import Session
+
+from city_app import models, schemas
+
+
+def create_city(db: Session, city: schemas.CityCreateSchema) -> models.City:
+    db_city = models.City(
+        name=city.name,
+        additional_info=city.additional_info
+    )
+    db.add(db_city)
+    db.commit()
+    db.refresh(db_city)
+    return db_city
+
+
+def get_city(db: Session, city_id: int) -> models.City:
+    city = db.query(models.City).filter(models.City.id == city_id).first()
+    return city
+
+
+def get_cities_list(db: Session) -> list[models.City]:
+    return db.query(models.City).all()
+
+
+def delete_city(db: Session, city_id: int) -> None:
+    db.query(models.City).filter(models.City.id == city_id).delete()
+    db.commit()
