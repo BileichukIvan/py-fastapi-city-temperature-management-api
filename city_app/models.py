a4a82d7786aab5edbe841c37e97e541aaedd9549
ivanb <bileichuk.ivan@gmail.com>
2024-08-28 13:24:35 +0300

    Aded models for city & temperature apps

---
+++ city_app/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    Column,
+    Integer,
+    String
+)
+from database import Base
+
+
+class City(Base):
+    __tablename__ = "city"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, unique=True)
+    additional_info = Column(String)
+    temperatures = relationship("Temperature", back_populates="city")
+
+    def __repr__(self):
+        return self.name
