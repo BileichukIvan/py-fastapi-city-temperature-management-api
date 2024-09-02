22236b69f5c70d90f6a6b3c35d9c25d058d5f3a9
ivanb <bileichuk.ivan@gmail.com>
2024-09-02 17:27:33 +0300

    Solution_v2

--- alembic/env.py
+++ alembic/env.py
@@ -5,8 +5,6 @@ from sqlalchemy import pool
 
 from alembic import context
 from database import Base
-from city_app.models import *
-from temperature_app.models import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- dependencies.py
+++ dependencies.py
@@ -1,9 +1,9 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import SessionLocal
 
 
-def get_db() -> Session:
+def get_db() -> AsyncSession:
     db = SessionLocal()
     try:
         yield db
